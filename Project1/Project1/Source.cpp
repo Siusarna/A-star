fa86de709c0c822d09b3e987ab450ef0df07e44f
Siusarna <charodeyrap@gmail.com>
2019-05-12 20:49:12 +0300

    Lab complite

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -3,6 +3,7 @@
 #include <list>
 #include <string>
 #include "a-star.hpp"
+#include "greedy.hpp"
 #include <Windows.h>
 
 using namespace std;
@@ -59,22 +60,22 @@ Kilometres **createMatrix(Kilometres **&sw) {
 		}
 	}
 	
-	sw[0][5] = 166; sw[0][6] = 300; sw[0][7] = 327; sw[0][8] = 244; sw[0][10] = 469;
-	sw[0][11] = 384; sw[0][12] = 169; sw[0][13] = 279; sw[0][14] = 313; sw[1][3] = 65.8;
-	sw[1][5] = 189; sw[1][6] = 281; sw[1][7] = 293; sw[1][8] = 210; sw[1][9] = 120;
-	sw[1][10] = 458; sw[1][11] = 403; sw[1][12] = 212; sw[1][13] = 271; sw[1][14] = 355;
-	sw[2][4] = 155; sw[2][5] = 229; sw[2][6] = 321; sw[2][7] = 333; sw[2][8] = 250;
-	sw[2][9] = 201; sw[2][10] = 498; sw[2][11] = 443; sw[2][12] = 235; sw[2][13] = 311;
-	sw[2][14] = 79; sw[3][4] = 174; sw[3][5] = 194; sw[3][6] = 328; sw[3][7] = 351;
-	sw[3][8] = 268; sw[3][9] = 169; sw[3][10] = 483; sw[3][11] = 397; sw[3][12] = 197;
-	sw[3][13] = 402; sw[3][14] = 349; sw[4][7] = 179; sw[4][10] = 344; sw[4][11] = 290;
-	sw[4][12] = 194; sw[4][13] = 158; sw[4][14] = 276; sw[5][6] = 201; sw[5][7] = 232;
-	sw[5][8] = 210; sw[5][10] = 312; sw[5][11] = 226; sw[5][13] = 180; sw[5][14] = 183;
-	sw[6][8] = 167; sw[6][9] = 160;  sw[6][10] = 184; sw[6][11] = 129; sw[6][12] = 240;
-	sw[6][14] = 212; sw[7][8] = 142;  sw[7][9] = 189; sw[7][10] = 222; sw[7][12] = 266;
-	sw[7][14] = 248; sw[8][10] = 351; sw[8][10] = 351; sw[8][11] = 296; sw[8][12] = 274;
-	sw[8][13] = 170; sw[8][14] = 309; sw[9][10] = 362; sw[9][11] = 261; sw[9][14] = 228;
-	sw[10][13] = 190; sw[11][12] = 215; sw[11][13] = 127; sw[12][13] = 214;
+	sw[0][5] = 115; sw[0][6] = 261; sw[0][7] = 272; sw[0][8] = 204; sw[0][10] = 378;
+	sw[0][11] = 300; sw[0][12] = 132; sw[0][13] = 245; sw[0][14] = 250; sw[1][3] = 52;
+	sw[1][5] = 112; sw[1][6] = 242; sw[1][7] = 253; sw[1][8] = 167; sw[1][9] = 90;
+	sw[1][10] = 368; sw[1][11] = 293; sw[1][12] = 147; sw[1][13] = 230; sw[1][14] = 258;
+	sw[2][4] = 136; sw[2][5] = 149; sw[2][6] = 280; sw[2][7] = 290; sw[2][8] = 200;
+	sw[2][9] = 128; sw[2][10] = 407; sw[2][11] = 331; sw[2][12] = 178; sw[2][13] = 268;
+	sw[2][14] = 293; sw[3][4] = 150; sw[3][5] = 140; sw[3][6] = 284; sw[3][7] = 300;
+	sw[3][8] = 219; sw[3][9] = 127; sw[3][10] = 403; sw[3][11] = 332; sw[3][12] = 157;
+	sw[3][13] = 269; sw[3][14] = 276; sw[4][7] = 154; sw[4][10] = 284; sw[4][11] = 216;
+	sw[4][12] = 141; sw[4][13] = 142; sw[4][14] = 214; sw[5][6] = 153; sw[5][7] = 178;
+	sw[5][8] = 148; sw[5][10] = 262; sw[5][11] = 184; sw[5][13] = 133; sw[5][14] = 148;
+	sw[6][8] = 131; sw[6][9] = 157;  sw[6][10] = 141; sw[6][11] = 94; sw[6][12] = 185;
+	sw[6][14] = 158; sw[7][8] = 116;  sw[7][9] = 176; sw[7][10] = 161; sw[7][12] = 218;
+	sw[7][14] = 200; sw[8][10] = 271; sw[8][11] = 220; sw[8][12] = 205;
+	sw[8][13] = 140; sw[8][14] = 250; sw[9][10] = 278; sw[9][11] = 203; sw[9][14] = 176;
+	sw[10][13] = 142; sw[11][12] = 187; sw[11][13] = 81; sw[12][13] = 161;
 	
 	for (int i = 0; i < 15; i++) {
 		for (int j = i; j < 15; j++) {
@@ -117,20 +118,41 @@ int main() {
 	}
 	Kilometres **routes = createMatrix(sw);
 	int distance = 0;
+	int distance1 = 0;
 	for (auto start : names) {
 		for (auto end : names) {
 			if (start == end) continue;
 			distance = 0;
+			distance1 = 0;
 			list<City> route = aStar<City, Kilometres, 15>(start, end, names, routes, sw, distance ,straight_way);
+			list<City> route1 = greedy<City, Kilometres, 15>(start, end, names, routes, sw, distance1, straight_way);
 			cout << "From " << start << " To " << end << " Distance " << distance << " Route: " << endl;
 			for (auto stop : route) {
 				cout << stop << " -> ";
 			}
 			cout << endl;
+			cout << "From " << start << " To " << end << " Distance " << distance1 << " Route: " << endl;
+			for (auto stop : route1) {
+				cout << stop << " -> ";
+			}
+			cout << endl;
+			cout << endl;
 		
 		}
 	}
+	/*for (auto start : names) {
+		for (auto end : names) {
+			if (start == end) continue;
+			distance = 0;
+			list<City> route = greedy<City, Kilometres, 15>(start, end, names, routes, sw, distance, straight_way);
+			cout << "From " << start << " To " << end << " Distance " << distance << " Route: " << endl;
+			for (auto stop : route) {
+				cout << stop << " -> ";
+			}
+			cout << endl;
 
+		}
+	}*/
 	
 	cout << endl;
 	system("pause");
---
+++ Project1/Project1/greedy.hpp
@@ -0,0 +1,110 @@
+#pragma once
+
+#include <array>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <unordered_map>
+#include <queue>
+#include <utility>
+
+
+using namespace std;
+
+template<typename Node, typename Cost, size_t N>
+std::list<Node> greedy(
+	const Node &START,
+	const Node &GOAL,
+	const Node vertex[N],
+	Cost **graph,
+	Cost **sw,
+	int &distance,
+	Cost(*heuristic)(Node current, Node goal, Cost **sw));
+
+template<typename T>
+bool contains2(std::deque<T> queue, T node) {
+	for (auto item : queue) {
+		if (item == node) {
+			return true;
+		}
+		else { return false; }
+	}
+}
+
+template<typename Node, size_t N>
+size_t index_of2(Node node, const Node nodes[N]) {
+	for (size_t n = 0; n < N; n++) {
+		if (nodes[n] == node) { return n; }
+	}
+}
+
+
+template<typename T>
+bool check2(priority_queue<T, std::vector<T>, std::greater<T>> frontier, int test) {
+	int temp = frontier.size();
+	for (int i = 0; i < temp; i++) {
+		if (test == frontier.top().second) return true;
+		frontier.pop();
+	}
+	return false;
+}
+
+template<typename Node, typename Cost, size_t N>
+std::list<Node> greedy(
+	const Node &START,
+	const Node &GOAL,
+	const Node vertex[N],
+	Cost **graph,
+	Cost **sw,
+	int &distance,
+	Cost(*heuristic)(Node current, Node goal, Cost **sw))
+{
+	int i = 0;
+	typedef std::pair<Cost, size_t> El;
+	std::priority_queue<El, std::vector<El>, std::greater<El>> frontier, temp;
+	frontier.emplace(0, index_of2<Node, N>(START, vertex));
+
+	std::deque<size_t> explored;
+
+	std::unordered_map<size_t, size_t> parent;
+
+	std::unordered_map<size_t, Cost> aggregateCost;
+	aggregateCost[frontier.top().second] = 0;
+
+	size_t current;
+
+	while (!frontier.empty()) {
+		current = frontier.top().second;
+		if (vertex[current] == GOAL) {
+			std::list<Node> path;
+
+			while (vertex[current] != START) {
+				path.push_front(vertex[current]);
+				distance += graph[current][parent[current]];
+				current = parent[current];
+			}
+			path.push_front(START);
+			return path;
+		}
+
+		explored.push_front(frontier.top().second);
+		frontier.pop();
+
+		for (size_t n = 0; n < N; n++) {
+
+			if (graph[current][n] == 0 || contains2(explored, n)) { continue; }
+
+			Cost cost = aggregateCost[current] + graph[current][n];
+
+			if (aggregateCost.find(n) == aggregateCost.end() || cost < aggregateCost[n]) {
+				parent[n] = current;
+				aggregateCost[n] = cost;
+				if (!check<El>(frontier, n)) {
+					frontier.emplace(heuristic(vertex[n], GOAL, sw), n);
+				}
+			}
+		}
+	}
+	list<Node> a;
+	return a;
+}
